9e9404ad55e1dd91ecf331f23f9296888de9ffad
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-08 18:55:14 +0200

    choose cloudflare adapter if build for is set

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import nodeAdapter from '@sveltejs/adapter-node';
+import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const getAdapter = (env) => {
+	if (env && env.toUpperCase() === 'CLOUDFLARE') {
+		return cloudflareAdapter();
+	}
+
+	return nodeAdapter({
+		dynamic_origin: true
+	});
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,9 +22,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			dynamic_origin: true
-		})
+		adapter: getAdapter(process.env.BUILD_FOR)
 	}
 };
 
