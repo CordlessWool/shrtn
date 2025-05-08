dc8bdcc9dcf093fabd79a9f7b8a3d16e869382f6
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-08 23:42:37 +0200

    switch to mailgun api

--- src/lib/server/mail/index.ts
+++ src/lib/server/mail/index.ts
@@ -1,33 +1,13 @@
-import nodemailer from 'nodemailer';
 import mustache from 'mustache';
 import juice from 'juice';
-import { env } from '$env/dynamic/private';
 import { THEME } from '$lib/helper/defaults';
 import VERIFICATION_TEMPLATE from './templates/verification.html?raw';
 import * as m from '$lib/paraglide/messages';
+import { initMailgun } from './provider/mailgun';
+import { env } from '$env/dynamic/private';
 
-const connection = {
-	host: env.MAIL_HOST,
-	port: env.MAIL_PORT,
-	secure: env.MAIL_SECURE ?? true,
-	auth: {
-		user: env.MAIL_USER,
-		pass: env.MAIL_PASS
-	}
-};
-
-const transporter = nodemailer.createTransport(
-	encodeURI(`smtps://${connection.auth.user}:${connection.auth.pass}@${connection.host}`)
-);
-
-export const mail = async (to: string, subject: string, html: string) => {
-	await transporter.sendMail({
-		from: env.MAIL_FROM,
-		to,
-		subject,
-		html
-	});
-};
+const provider = initMailgun();
+const defaultFrom = env.MAIL_FROM;
 
 export const sendVerificationMail = async (to: string, key: string, theme: THEME = THEME.DARK) => {
 	const html = mustache.render(VERIFICATION_TEMPLATE, {
@@ -39,5 +19,10 @@ export const sendVerificationMail = async (to: string, key: string, theme: THEME
 		key
 	});
 	const htmlWithInlineStyles = juice(html);
-	await mail(to, m.verification(), htmlWithInlineStyles);
+	await provider.mail({
+		from: defaultFrom,
+		to,
+		subject: m.verification(),
+		html: htmlWithInlineStyles
+	});
 };
---
+++ src/lib/server/mail/provider/mailgun.ts
@@ -0,0 +1,25 @@
+import Mailgun from 'mailgun.js';
+import { Interfaces } from 'mailgun.js/definitions';
+import { env } from '$env/dynamic/private';
+
+const mail = (client: Interfaces.IMailgunClient) => {
+	const domain = env.MAILGUN_DOMAIN;
+	return async (data: { from: string; to: string; subject: string; html: string }) => {
+		const response = await client.messages.create(domain, data);
+		if (response.status >= 400) {
+			throw new Error('Error on sending mail with Mailgun');
+		}
+	};
+};
+
+export const initMailgun = () => {
+	const mailgun = new Mailgun(FormData);
+	const client = mailgun.client({
+		username: 'api',
+		key: env.MAILGUN_API_KEY!,
+		url: env.MAILGUN_URL
+	});
+	return {
+		mail: mail(client)
+	};
+};
