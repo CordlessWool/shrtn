73ffe2ceeea58bc11c0ef18efb3812fe8b0c11c3
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-18 17:43:18 +0200

    Update docker.sh

--- e2e/scripts/docker.sh
+++ e2e/scripts/docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-curl http://localhost:4173
+
 
 # Wait for it to be ready
 until curl --silent --output /dev/null --fail http://localhost:4173; do
   sleep 3
+  curl -v http://localhost:4173
 done
 
 bun playwright test
