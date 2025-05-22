0968addd12b7cb09e0cf9b10e09edf58c41e7ad3
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-23 00:12:01 +0200

    wait for login page

--- e2e/scripts/smtp.sh
+++ e2e/scripts/smtp.sh
@@ -10,9 +10,9 @@ SERVER_PID=$!
 echo $SERVER_PID
 
 # Wait for it to be ready
-until curl --silent --output /dev/null --fail http://localhost:4173; do
+until curl --silent --output /dev/null --fail http://localhost:4173/login; do
   sleep 3
 done
 
 bun playwright test e2e/login.test.ts
-kill $SERVER_PID
+kill -9 $SERVER_PID
