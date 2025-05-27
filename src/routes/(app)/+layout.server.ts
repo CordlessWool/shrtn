25260a251e939e60bc1d6d74f56ae3b7711df33d
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-05-27 16:27:16 +0200

    remove logs

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -4,8 +4,6 @@ import { redirect } from '@sveltejs/kit';
 import { privateInstance } from '$lib/helper/env';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	console.log({ locals });
-	console.log(isAuthenticated(locals), privateInstance);
 	if (privateInstance && !isAuthenticated(locals)) {
 		// Redirect to login page
 		redirect(302, '/login');
