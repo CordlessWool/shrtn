27a1ec7c89d5f6d4f2d856d907ddaaf86fafa24e
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-06-02 19:54:51 +0200

    fix call links on private instances

---
+++ .changeset/stupid-crews-hide.md
@@ -0,0 +1,5 @@
+---
+'shrtn.to': patch
+---
+
+Allow to access links public on private instance
--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -3,8 +3,8 @@ import { isAuthenticated } from '$lib/helper/auth.server';
 import { redirect } from '@sveltejs/kit';
 import { privateInstance } from '$lib/helper/env';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	if (privateInstance && !isAuthenticated(locals)) {
+export const load: LayoutServerLoad = async ({ locals, route }) => {
+	if (privateInstance && !isAuthenticated(locals) && route.id !== '/(app)/[shorten]') {
 		// Redirect to login page
 		redirect(302, '/login');
 	}
